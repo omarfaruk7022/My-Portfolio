6998342a13996af5f3a83c2bd5ba2e6a9930f082
omarfaruk7022 <farhanfaruk221@gmail.com>
2023-07-10 17:24:56 +0600

    responsive fixed

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <>
       <Navbar />
-      <div className="px-0 lg:px-20 xl:px-72  grid grid-cols-12 md:gap-10 justify-between lg:mt-[100px]">
+      <div className="px-0  lg:px-36   grid grid-cols-12 md:gap-10 justify-between lg:mt-[100px]">
         <div className="col-span-12 lg:col-span-4  lg:block  h-screen lg:sticky lg:top-36 mb-36">
           <FixedDetails />
         </div>

2c3e2cb14bc5b73e99150a862d6da2e39623fa5d
omarfaruk7022 <farhanfaruk221@gmail.com>
2023-07-16 17:01:21 +0600

    minor change in project

--- Components/Home/Routing.js
+++ Components/Home/Routing.js
@@ -7,7 +7,7 @@ export default function Routing() {
   const router = useRouter();
   return (
     <div>
-      <div className="bg-white dark:bg-[#111111]  rounded-lg flex p-5 gap-1 w-[200px] ml-28 mt-28">
+      <div className="bg-white dark:bg-[#111111]  rounded-lg flex p-5 gap-1 w-[200px] ml-28 mt-0 lg:mt-28">
         <Link
           href={"/about"}
           className={`bg-[#F3F6F6] dark:bg-[#1D1D1D] rounded-lg p-2 w-28 text-center hover:bg-gradient-to-l to-[#23CE6B] from-[#286e45] hover:text-white ${

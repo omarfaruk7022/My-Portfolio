cf77cf36ab5c7df3b28c39473c4bcdbf39326787
omarfaruk7022 <farhanfaruk3221@gmail.com>
2023-09-14 11:34:36 +0600

    project updated

--- Components/Home/Projects.js
+++ Components/Home/Projects.js
@@ -3,7 +3,7 @@ import React from "react";
 import idolgroup from "../../assets/Images/idolgroup.png";
 import sfs from "../../assets/Images/sfs.png";
 import regularShop from "../../assets/Images/regularshop.png";
-import prime from "../../assets/Images/prime.png";
+import bus from "../../assets/Images/bus365.png";
 import { LuExternalLink } from "react-icons/lu";
 import Link from "next/link";
 import Footer from "./Footer";
@@ -19,6 +19,42 @@ export default function Projects() {
           <div className="border border-green-400 w-28 ml-14 "></div>
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2  gap-8">
+          {" "}
+          <div className="bg-[#F8FBFB] dark:bg-[#1a1a1a] rounded-lg shadow-md">
+            <div className="overflow-scroll h-96 w-full scrollbar-hide cursor-pointer">
+              <Image src={bus} alt=""></Image>
+            </div>
+            <div className="p-5">
+              <h2 className="text-2xl text-transparent bg-clip-text bg-gradient-to-l to-[#0d9244] from-[#57e793]">
+                Bus365
+              </h2>
+              <p className="text-xs text-black dark:text-[#d8d8d8]">
+                Bus365 : Bus Reservation System with Website is a very wonderful
+                Bus and fleet Booking System, This Bus reservation system is
+                developed by PHP and Code Igniter & frontend developed by React
+                js.It is mostly useful for any Bus, Fleet and Ticket Selling
+                Company.It has a variety of features on Bus ticket Management.
+              </p>
+              <div className="flex justify-between items-center pt-4">
+                <Link
+                  className=" text-green-400 flex items-center text-sm   hover:text-green-500"
+                  href="https://bus365demo.bdtask-demo.com/"
+                  target="_blank"
+                >
+                  Preview
+                  <LuExternalLink className="inline-block ml-2" />
+                </Link>
+                <Link
+                  className=" text-green-400 flex items-center text-sm   hover:text-green-500"
+                  href="https://bus365demo.bdtask-demo.com/backend/login"
+                  target="_blank"
+                >
+                 Admin panel
+                  <LuExternalLink className="inline-block ml-2" />
+                </Link>
+              </div>
+            </div>
+          </div>
           <div className="bg-[#F8FBFB] dark:bg-[#1a1a1a] rounded-lg shadow-md">
             <div className="overflow-scroll h-96 w-full scrollbar-hide cursor-pointer">
               <Image src={idolgroup} alt=""></Image>
@@ -79,10 +115,10 @@ export default function Projects() {
                 Regular Shop
               </h2>
               <p className="text-xs text-black dark:text-[#d8d8d8]">
-                A Online market and it is a Personal project, Front end created with nextjs, tailwind CSS,
-                 User and admin role available.Only admin can
-                change roles of users Authentication with firebase, and database
-                with mongoDB using mongoose.
+                A Online market and it is a Personal project, Front end created
+                with nextjs, tailwind CSS, User and admin role available.Only
+                admin can change roles of users Authentication with firebase,
+                and database with mongoDB using mongoose.
               </p>
               <div className="flex justify-between items-center pt-4">
                 <Link
@@ -112,51 +148,9 @@ export default function Projects() {
               </div>
             </div>
           </div>
-          <div className="bg-[#F8FBFB] dark:bg-[#1a1a1a] rounded-lg shadow-md">
-            <div className="overflow-scroll h-96 w-full scrollbar-hide cursor-pointer">
-              <Image src={prime} alt=""></Image>
-            </div>
-            <div className="p-5">
-              <h2 className="text-2xl text-transparent bg-clip-text bg-gradient-to-l to-[#0d9244] from-[#57e793]">
-                Prime Candidates
-              </h2>
-              <p className="text-xs text-black dark:text-[#d8d8d8]">
-                A Hiring agency application, Made with Nextjs, with Responsive
-                and exclusive design. Payment implemented with stripe, secured
-                routing,Every user has permission to see some routes not all in
-                Dashboard. .
-              </p>
-              <div className="flex justify-between items-center pt-4">
-                <Link
-                  className=" text-green-400 flex items-center text-[10px]  lg:text-[12px]   hover:text-green-500"
-                  href="https://prime-candidates-client.vercel.app/"
-                  target="_blank"
-                >
-                  Preview
-                  <LuExternalLink className="inline-block ml-2" />
-                </Link>
-                <Link
-                  className=" text-green-400 flex items-center text-[10px]  lg:text-[12px]   hover:text-green-500"
-                  href="https://github.com/omarfaruk7022/prime-candidates-client"
-                  target="_blank"
-                >
-                  Github Client
-                  <LuExternalLink className="inline-block ml-2" />
-                </Link>
-                <Link
-                  className=" text-green-400 flex items-center text-[10px]  lg:text-[12px]   hover:text-green-500"
-                  href="https://github.com/dev-weirdo/prime-candidates-backend"
-                  target="_blank"
-                >
-                  Github Server
-                  <LuExternalLink className="inline-block ml-2" />
-                </Link>
-              </div>
-            </div>
-          </div>
         </div>
       </div>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
